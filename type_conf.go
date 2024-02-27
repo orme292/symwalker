5c1d343f951bfba5de83a109f8b4baa190ecdd08
orme292 <andrew.orme@me.com>
2024-02-26 21:56:36 -0500

    Framework 2a

--- type_conf.go
+++ type_conf.go
@@ -20,6 +20,7 @@ func (c *Conf) GlobCheck() error {
 	// GlobPattern must have a value, default is to match all files ("*")
 	if c.GlobPattern == emptyString {
 		c.GlobPattern = GlobMatchAll
+		return nil
 	}
 	_, err := filepath.Match(c.GlobPattern, "/users/sample/grow/451614/!$!$!")
 	// filepath.Match only throws one type of error, filepath.ErrBadPattern
