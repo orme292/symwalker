d6197893aee7a53700be43dd66181e81b37a303c
orme292 <andrew.orme@me.com>
2024-04-17 16:22:10 -0400

    marked should not be exported

--- type_direntry.go
+++ type_direntry.go
@@ -1,9 +1,9 @@
 package symwalker
 
 // DirEntry holds directory entry information.
-// Marked is a flag used to indicate whether the DirEntry
+// marked is a flag used to indicate whether the DirEntry
 // has been processed/walked.
 type DirEntry struct {
 	Path   string
-	Marked bool
+	marked bool
 }
