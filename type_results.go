49b706f245bdd1a7161002a6e280ae5edad46f15
orme292 <andrew.orme@me.com>
2024-04-18 12:42:55 -0400

    add package comment

--- type_results.go
+++ type_results.go
@@ -1,3 +1,7 @@
+// Package symwalker is a directory tree walker with symlink loop protection.
+// It builds a separate history for each branching sub-directory below
+// a given starting path. When evaluating a new symlink that targets a
+// directory, the branch history is checked before walking the directory.
 package symwalker
 
 // DirEntries holds each directory entry.
