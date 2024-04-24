b29b6830a97bb32d11fbbd0d7f8ab680d8e7a500
Andrew Orme <orme292@users.noreply.github.com>
2024-04-23 23:32:21 -0400

    Add objf.FileObj field to DirEntry
    Signed-off-by: orme292 <orme292@users.noreply.github.com>

--- type_direntry.go
+++ type_direntry.go
@@ -1,9 +1,14 @@
 package symwalker
 
+import (
+	objf "github.com/orme292/objectify"
+)
+
 // DirEntry holds directory entry information.
 // marked is a flag used to indicate whether the DirEntry
 // has been processed/walked.
 type DirEntry struct {
-	Path   string
-	marked bool
+	Path    string
+	FileObj *objf.FileObj
+	marked  bool
 }
